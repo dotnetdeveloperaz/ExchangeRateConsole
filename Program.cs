9a25fc06bacf42e4d7706887c5093b95b1b8e035
Scott Glasgow <dotnetdeveloperaz@gmail.com>
2023-04-13 00:37:18 +0300

    Moved configuration into it's own code area

--- Program.cs
+++ Program.cs
@@ -1,7 +1,4 @@
-﻿using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Logging.Console;
-using Microsoft.Extensions.Logging.Debug;
-using Spectre.Console;
+﻿using Spectre.Console;
 using Spectre.Console.Cli;
 using ExchangeRateConsole.Models;
 
@@ -11,21 +8,9 @@ class Program
 {
     static Configuration configuration = new();
     static Exchange exchange = new();
-
+ 
     public static async Task Main(string[] args)
     {
-        var loggerFactory = LoggerFactory.Create(
-            builder => builder
-//                .AddConsole();
-//                .AddDebug()
-//                .SetMinimumLevel(LogLevel.Debug)
-                .AddFilter("Microsoft", LogLevel.Warning)
-                .AddFilter("System", LogLevel.Warning)
-                .AddFilter("ExchangeRateConsole.Program", LogLevel.Debug)
-        );
-
-        var logger = loggerFactory.CreateLogger<Program>();
-        logger.LogInformation("Exchange Rate Console Starting Up");
         Title.Print();
         configuration = Configure.Load(configuration);
         var app = CommandApplication.Initialize();
@@ -35,7 +20,6 @@ class Program
         }
         catch (Exception ex)
         {
-            logger.LogError($"Spectre.Console.App.RunAsync threw an error. {ex.Message}");
             AnsiConsole.WriteException(ex, ExceptionFormats.ShortenEverything);
         }
     }
