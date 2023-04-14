1024ea412dc223e6600d13b5be1228f8a565b83f
Scott Glasgow <dotnetdeveloperaz@gmail.com>
2023-04-14 15:06:48 +0300

    Added code for GetRate to see if the date is a holiday or weekend

--- Utility.cs
+++ Utility.cs
@@ -1,11 +1,23 @@
+using Newtonsoft.Json;
 using PublicHoliday;
+using ExchangeRateConsole.Models;
 
 namespace ExchangeRateConsole;
 
 public class Utility
 {
     private static List<DateTime> listOfDays;
-    public List<DateTime> ListOfDays { get { return listOfDays; } }
+    private static Exchange exchangeRate;
+
+    public List<DateTime> ListOfDays
+    {
+        get { return listOfDays; }
+    }
+
+    public Exchange ExchangeRateConsole
+    {
+        get { return exchangeRate; }
+    }
 
     public static int GetNumberOfDays(DateTime start, DateTime end)
     {
@@ -13,14 +25,6 @@ public class Utility
         int i = 0;
         var res = DateTime.Compare(end, DateTime.Now);
 
-        // We do not want the end date to be the current date or future date.
-/*
-        if (res >= 0)
-        {
-            end = DateTime.Now.AddDays(-1);
-            endDate = end;
-        }
-*/
         while (start <= end)
         {
             bool isHoliday = new USAPublicHoliday().IsPublicHoliday(start);
@@ -29,10 +33,34 @@ public class Utility
                 && start.DayOfWeek != DayOfWeek.Saturday
                 && start.DayOfWeek != DayOfWeek.Sunday
             )
+            
             i++;
             listOfDays.Add(start);
             start = start.AddDays(1);
         }
         return i;
+    }
+
+    public static bool IsHolidayOrWeekend(string Date)
+    {
+        DateTime date = DateTime.Parse(Date);
+        bool isHoliday = new USAPublicHoliday().IsPublicHoliday(date);
+        if (isHoliday || date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday)
+            return true;
+        else
+            return false;
+    }
+
+    public static Exchange GetExchangeRate(string Uri, bool Save)
+    {
+        var client = new HttpClient();
+        var response = client.GetAsync(Uri).GetAwaiter().GetResult();
+        var info = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+        exchangeRate = JsonConvert.DeserializeObject<Exchange>(info);
+
+//        string result = JsonConvert.SerializeObject(exchangeRate, Formatting.Indented);
+//        File.AppendAllText($"OneDayRate.sample", result);
+
+        return exchangeRate;
     }
 }
