a7b96d57c5eb8e4de4bf024b1a07437b3b155162
Scott Glasgow <dotnetdeveloperaz@gmail.com>
2023-04-17 13:43:35 +0300

    Non working code in progress. Checking in to be able to pick up on another machine

---
+++ GlobalUsings.cs
@@ -0,0 +1,7 @@
+global using System.Diagnostics.Tracing;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Logging;
+global using Microsoft.Extensions.Logging.Console;
+global using Spectre.Console;
+global using Spectre.Console.Cli;
