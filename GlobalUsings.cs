cf6d2ac2335d8238bb87f5383bab127f0c21f3dc
Scott Glasgow <=>
2023-04-25 23:25:20 -0700

    Fixed issue with the account information URL call.

--- GlobalUsings.cs
+++
@@ -1,6 +0,0 @@
-global using Microsoft.Extensions.Configuration;
-global using Microsoft.Extensions.DependencyInjection;
-global using Microsoft.Extensions.Logging;
-global using Spectre.Console;
-global using Spectre.Console.Cli;
-global using System.Diagnostics.Tracing;
