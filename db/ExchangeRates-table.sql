2c5d530164b3ea5a00809b4076b53ee734c65eb0
Scott Glasgow <dotnetdeveloperaz@gmail.com>
2023-06-09 15:04:15 +0300

    Updated database to store base Symbol used to get Exchange Rates

--- db/ExchangeRates-table.sql
+++ db/ExchangeRates-table.sql
@@ -3,8 +3,10 @@
 CREATE TABLE `ExchangeRates` (
   `Id` mediumint(9) NOT NULL AUTO_INCREMENT,
   `Symbol` char(3) NOT NULL,
+  `BaseSymbol` char(3) NOT NULL,
   `Rate` float NOT NULL,
   `RateDate` date NOT NULL,
   `AddDate` timestamp NOT NULL DEFAULT current_timestamp(),
-  PRIMARY KEY (`Id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4655 DEFAULT CHARSET=utf8mb4;
+  PRIMARY KEY (`Id`),
+  UNIQUE KEY `ExchangeRates_UN` (`Symbol`, `BaseSymbol`, `RateDate`)
+) ENGINE=InnoDB AUTO_INCREMENT=12827 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
--- db/usp_AddExchangeRate.sql
+++ db/usp_AddExchangeRate.sql
@@ -1,8 +1,8 @@
 CREATE DEFINER=`root`@`%` PROCEDURE `Personal`.`usp_AddExchangeRate`(
-	IN symbol CHAR(3), IN rate float, IN ratedate date)
+	IN symbol CHAR(3), IN baseSymbol CHAR(3), IN rate float, IN ratedate date)
 BEGIN
 	INSERT INTO ExchangeRates (Symbol, Rate, RateDate)
-	VAlUES (symbol, rate, ratedate)
+	VAlUES (symbol, baseSymbol, rate, ratedate)
 	ON DUPLICATE KEY UPDATE 
 	Rate = rate;
 END;
