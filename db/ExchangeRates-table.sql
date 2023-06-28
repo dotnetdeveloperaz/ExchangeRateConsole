8ef1539ee699b93de8ef2c2bf59132467329154d
Scott Glasgow <dotnetdeveloperaz@gmail.com>
2023-06-28 11:14:34 +0300

    Updated table script for new table column to store last update.

--- db/ExchangeRates-table.sql
+++ db/ExchangeRates-table.sql
@@ -7,6 +7,7 @@ CREATE TABLE `ExchangeRates` (
   `Rate` float NOT NULL,
   `RateDate` date NOT NULL,
   `AddDate` timestamp NOT NULL DEFAULT current_timestamp(),
+  `Last_Update` timestamp NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`Id`),
   UNIQUE KEY `ExchangeRates_UN` (`Symbol`, `BaseSymbol`, `RateDate`)
 ) ENGINE=InnoDB AUTO_INCREMENT=12827 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
