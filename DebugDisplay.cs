f1cec622e40e3deb55deaed5d16b9204de2ef277
unknown <dotnetdeveloperaz@gmail.com>
2024-01-01 18:15:45 -0700

    Added new class to handle debug data display to eliminate the same code everywhere

---
+++ DebugDisplay.cs
@@ -0,0 +1,70 @@
+﻿using System.Reflection;
+using Spectre.Console.Cli;
+using Spectre.Console;
+using ExchangeRateConsole.Models;
+
+namespace ExchangeRateConsole
+{
+    internal class DebugDisplay
+    {
+        internal static bool Print(CommandSettings settings, ApiServer server, string Url)
+        {
+            return Print(settings, server, "", Url);
+        }
+        internal static bool Print(CommandSettings settings, ApiServer server, string connectionString, string Url)
+        {
+            // Debug Window
+            var table = new Table().Centered();
+            table.BorderColor(Color.BlueViolet);
+            table.Border(TableBorder.DoubleEdge);
+            table.Expand();
+
+            // Columns
+            table.AddColumn("Setting Key");
+            table.AddColumn("Value");
+
+            // Column alignment
+            table.Columns[0].RightAligned();
+            table.Columns[1].RightAligned();
+
+            bool isDebug = false;
+            bool showHidden = false;
+            PropertyInfo[] properties = settings.GetType().GetProperties();
+            foreach (PropertyInfo property in properties)
+            {
+                object value = property.GetValue(settings);
+                if (property.Name == "Debug")
+                    isDebug = (bool)value;
+                if (property.Name == "ShowHidden")
+                    showHidden = (bool)value;
+            }
+            if (isDebug)
+            {
+                if (showHidden)
+                {
+                    if (connectionString != String.Empty)
+                        table.AddRow($"Database Connection:", $"{connectionString}");
+                    table.AddRow($"AppId:", $"{server.AppId}");
+                    table.AddRow($"Url:", $"{Url}");
+                }
+                foreach (PropertyInfo property in properties)
+                {
+                    object value = property.GetValue(settings);
+
+                    if (property.PropertyType == typeof(bool))
+                        table.AddRow($"{property.Name}", $"[yellow]{(bool)value}[/]");
+                    else
+                        if (value is not null)
+                        table.AddRow($"{property.Name}", $"{value}");
+                }
+                AnsiConsole.Write(table);
+                if (AnsiConsole.Confirm("Continue?"))
+                    Title.Print();
+                else
+                    return false;
+
+            }
+            return true;
+        }
+    }
+}
