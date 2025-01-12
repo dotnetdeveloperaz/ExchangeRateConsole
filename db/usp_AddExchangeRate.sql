a583fe135fa9568461522acd553db044a08e755c
DotnetDeveloperAZ <dotnetdeveloperaz@gmail.com>
2025-01-12 11:51:28 -0700

    Adding new procs and updated one to include CREATE OR REPLACE.

--- db/usp_AddExchangeRate.sql
+++ db/usp_AddExchangeRate.sql
@@ -1,4 +1,5 @@
-CREATE PROCEDURE `Personal`.`usp_AddExchangeRate`(
+DELIMITER //
+CREATE OR REPLACE PROCEDURE `Personal`.`usp_AddExchangeRate`(
 	IN symbol CHAR(3), IN baseSymbol CHAR(3), IN rate float, IN ratedate date)
 BEGIN
 	INSERT INTO ExchangeRates (Symbol, Rate, RateDate)
@@ -6,3 +7,4 @@ BEGIN
 	ON DUPLICATE KEY UPDATE 
 	Rate = rate;
 END;
+//
---
+++ db/usp_GetExchangeRates.sql
@@ -0,0 +1,19 @@
+﻿DELIMITER //
+CREATE OR REPLACE PROCEDURE `personal`.`usp_GetExchangeRates`(
+	IN startDate varchar(10),
+	IN endDate varchar(10),
+	IN symbols varchar(15),
+	IN baseCurrency varchar(3)
+)
+BEGIN
+	SELECT 
+		Symbol
+		, Rate 
+		, RateDate
+	FROM ExchangeRates
+	WHERE RateDate >= startDate
+	AND RateDate <= endDate
+	AND FIND_IN_SET(Symbol, symbols) > 0
+	AND BaseSymbol = baseCurrency;
+END;
+//
